ac4f0ef6e6802d0863409ea2cedf684a6df11341
Mohamed Alaa <mohamedalaa127@gmail.com>
2023-05-12 13:19:06 -0700

    whenGetAll_returnJsonArray Method has been added

--- src/test/java/com/SpringBoot/Course/todoapi/todos/TodoControllerTest.java
+++ src/test/java/com/SpringBoot/Course/todoapi/todos/TodoControllerTest.java
@@ -3,7 +3,9 @@ package com.SpringBoot.Course.todoapi.todos;
 
 import com.SpringBoot.Course.springnoot.App;
 import com.SpringBoot.Course.springnoot.todos.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,9 +16,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
@@ -27,13 +37,23 @@ public class TodoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private TodoServices todoServices;
+
     @Test
-    public void testGetAll(){
-        try {
-            mockMvc.perform(get("/api/v1/todos")).andExpect(status().isOk());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public void whenGetAll_returnJsonArray() throws Exception{
+        Todo todo1 = new Todo("1", "task1", "this is task1");
+        Todo todo2 = new Todo("2", "task2", "this is task2");
+        List<Todo> todos = Arrays.asList(todo1, todo2);
+
+       when(todoServices.getAll()).thenReturn(todos);
+
+        mockMvc.perform(get("/api/v1/todos").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].title", equalTo(todo1.getTitle())))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(status().isOk());
     }
+
+
 
 }
