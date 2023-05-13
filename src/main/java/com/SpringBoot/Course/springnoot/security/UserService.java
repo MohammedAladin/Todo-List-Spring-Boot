55768fdbadf535b9bdc92708f54c952d10d30a6e
mohamed <Mohamedalaa127@gmail.com>
2023-05-13 19:39:43 +0300

    Security layer has been added (but it's not completed)

---
+++ src/main/java/com/SpringBoot/Course/springnoot/security/UserService.java
@@ -0,0 +1,26 @@
+package com.SpringBoot.Course.springnoot.security;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Bean
+    private PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("Mohammed", passwordEncoder().encode("password"), AuthorityUtils.NO_AUTHORITIES);
+    }
+}
